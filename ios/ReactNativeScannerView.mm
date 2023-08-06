a86d87726c656b6f69cc84e25015504a392f2175
luvnishCapgemini <132471357+luvnishCapgemini@users.noreply.github.com>
2023-08-06 20:56:52 +0530

    Added multiple QR code support for IOS (#8)
    Co-authored-by: Luvnish <luvnish.monga@intelex.com>

--- ios/ReactNativeScannerView.mm
+++ ios/ReactNativeScannerView.mm
@@ -35,27 +35,27 @@ using namespace facebook::react;
     _props = defaultProps;
 
     _view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 500, 500)];
-        
-        
+
+
     _session = [[AVCaptureSession alloc] init];
     _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     NSError *error = nil;
-        
+
     _input = [AVCaptureDeviceInput deviceInputWithDevice:_device error:&error];
     if (_input) {
         [_session addInput:_input];
     } else {
         NSLog(@"%@", [error localizedDescription]);
     }
-        
+
     _output = [[AVCaptureMetadataOutput alloc] init];
     [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
     [_session addOutput:_output];
-        
+
     _output.metadataObjectTypes = [_output availableMetadataObjectTypes];
-        
+
     _prevLayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
-    
+
     _prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
        [_view.layer addSublayer:_prevLayer];
        [_session startRunning];
@@ -74,25 +74,23 @@ using namespace facebook::react;
   NSArray *barCodeTypes = @[AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
                             AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
                             AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode];
-  
+
   for (AVMetadataObject *metadata in metadataObjects) {
     for (NSString *type in barCodeTypes) {
-      if ([metadata.type isEqualToString:type])
-      {
+      if ([metadata.type isEqualToString:type]) {
         barCodeObject = (AVMetadataMachineReadableCodeObject *)[_prevLayer transformedMetadataObjectForMetadataObject:(AVMetadataMachineReadableCodeObject *)metadata];
         highlightViewRect = barCodeObject.bounds;
         detectionString = [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
         break;
       }
     }
-    if (detectionString != nil)
-    {          if (_eventEmitter != nullptr) {
-      
-      std::dynamic_pointer_cast<const facebook::react::ReactNativeScannerViewEventEmitter>(_eventEmitter)->onQrScanned(facebook::react::ReactNativeScannerViewEventEmitter::OnQrScanned{
+    if (detectionString != nil)  {
+      if (_eventEmitter != nullptr) {
+
+        std::dynamic_pointer_cast<const facebook::react::ReactNativeScannerViewEventEmitter>(_eventEmitter)->onQrScanned(facebook::react::ReactNativeScannerViewEventEmitter::OnQrScanned{
         .value = std::string([detectionString UTF8String])
-      });
-    }
-      break;
+        });
+      }
     }
   }
 }

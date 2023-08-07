ad3e457dec0a1c34d38d4d3426c5b2c644bc918b
Pushpender Singh <pushpendersingh694@gmail.com>
2023-08-07 13:01:46 +0530

    refactor: remove unnecessary files and upgrade the example app

--- example/ios/reactnativescannerexampleTests/reactnativescannerexampleTests.m
+++ example/ios/reactnativescannerexampleTests/reactnativescannerexampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface reactnativescannerexampleTests : XCTestCase
+@interface ReactNativeScannerExampleTests : XCTestCase
 
 @end
 
-@implementation reactnativescannerexampleTests
+@implementation ReactNativeScannerExampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {

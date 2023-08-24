ed80a83a5c2e5a0240c6ccf75c2bbbf33604ac08
pushpender singh <pushpender.singh@capgemini.com>
2023-08-24 07:29:14 -0400

    Fetching coordinates for barcode

--- android/src/main/java/com/pushpendersingh/reactnativescanner/ReactNativeScannerView.kt
+++ android/src/main/java/com/pushpendersingh/reactnativescanner/ReactNativeScannerView.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.util.Log
 import android.view.Choreographer
 import android.view.ViewGroup
 import android.widget.LinearLayout
@@ -24,182 +25,191 @@ import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
+import java.util.Arrays
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 class ReactNativeScannerView(context: Context) :  LinearLayout(context) {
 
-    private var preview: PreviewView
-    private var mCameraProvider: ProcessCameraProvider? = null
-    private lateinit var cameraExecutor: ExecutorService
-    private lateinit var options: BarcodeScannerOptions
-    private lateinit var scanner: BarcodeScanner
-    private var analysisUseCase: ImageAnalysis = ImageAnalysis.Builder()
-        .build()
+  private var preview: PreviewView
+  private var mCameraProvider: ProcessCameraProvider? = null
+  private lateinit var cameraExecutor: ExecutorService
+  private lateinit var options: BarcodeScannerOptions
+  private lateinit var scanner: BarcodeScanner
+  private var analysisUseCase: ImageAnalysis = ImageAnalysis.Builder()
+    .build()
 
-    companion object {
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS =
-            mutableListOf(
-                Manifest.permission.CAMERA
-            ).toTypedArray()
-    }
+  companion object {
+    private const val REQUEST_CODE_PERMISSIONS = 10
+    private val REQUIRED_PERMISSIONS =
+      mutableListOf(
+        Manifest.permission.CAMERA
+      ).toTypedArray()
+  }
 
-    init {
-        val linearLayoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
-        layoutParams = linearLayoutParams
-        orientation = VERTICAL
+  init {
+    val linearLayoutParams = ViewGroup.LayoutParams(
+      ViewGroup.LayoutParams.WRAP_CONTENT,
+      ViewGroup.LayoutParams.WRAP_CONTENT
+    )
+    layoutParams = linearLayoutParams
+    orientation = VERTICAL
 
-        preview = PreviewView(context)
-        preview.layoutParams = ViewGroup.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.MATCH_PARENT
-        )
-        addView(preview)
+    preview = PreviewView(context)
+    preview.layoutParams = ViewGroup.LayoutParams(
+      ViewGroup.LayoutParams.MATCH_PARENT,
+      ViewGroup.LayoutParams.MATCH_PARENT
+    )
+    addView(preview)
 
-        setupLayoutHack()
+    setupLayoutHack()
+    manuallyLayoutChildren()
+  }
+
+  private fun setupLayoutHack() {
+    Choreographer.getInstance().postFrameCallback(object : Choreographer.FrameCallback {
+      override fun doFrame(frameTimeNanos: Long) {
         manuallyLayoutChildren()
+        viewTreeObserver.dispatchOnGlobalLayout()
+        Choreographer.getInstance().postFrameCallback(this)
+      }
+    })
+  }
+
+  private fun manuallyLayoutChildren() {
+    for (i in 0 until childCount) {
+      val child = getChildAt(i)
+      child.measure(
+        MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY),
+        MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY)
+      )
+      child.layout(0, 0, child.measuredWidth, child.measuredHeight)
+    }
+  }
+
+  fun setUpCamera(reactApplicationContext: ReactApplicationContext) {
+    if (allPermissionsGranted()) {
+      startCamera(reactApplicationContext)
     }
 
-    private fun setupLayoutHack() {
-        Choreographer.getInstance().postFrameCallback(object : Choreographer.FrameCallback {
-            override fun doFrame(frameTimeNanos: Long) {
-                manuallyLayoutChildren()
-                viewTreeObserver.dispatchOnGlobalLayout()
-                Choreographer.getInstance().postFrameCallback(this)
-            }
-        })
+    cameraExecutor = Executors.newSingleThreadExecutor()
+
+    options = BarcodeScannerOptions.Builder()
+      .setBarcodeFormats(
+        Barcode.FORMAT_QR_CODE,
+        Barcode.FORMAT_AZTEC,
+        Barcode.FORMAT_CODE_128,
+        Barcode.FORMAT_CODE_39,
+        Barcode.FORMAT_CODE_93,
+        Barcode.FORMAT_CODABAR,
+        Barcode.FORMAT_DATA_MATRIX,
+        Barcode.FORMAT_EAN_13,
+        Barcode.FORMAT_EAN_8,
+        Barcode.FORMAT_ITF,
+        Barcode.FORMAT_PDF417,
+        Barcode.FORMAT_UPC_A,
+        Barcode.FORMAT_UPC_E
+      )
+      .build()
+    scanner = BarcodeScanning.getClient(options)
+
+    analysisUseCase.setAnalyzer(
+      // newSingleThreadExecutor() will let us perform analysis on a single worker thread
+      Executors.newSingleThreadExecutor()
+    ) { imageProxy ->
+      processImageProxy(scanner, imageProxy)
     }
+  }
 
-    private fun manuallyLayoutChildren() {
-        for (i in 0 until childCount) {
-            val child = getChildAt(i)
-            child.measure(
-                MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY),
-                MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY)
-            )
-            child.layout(0, 0, child.measuredWidth, child.measuredHeight)
-        }
-    }
+  @SuppressLint("UnsafeOptInUsageError")
+  private fun processImageProxy(
+    barcodeScanner: BarcodeScanner,
+    imageProxy: ImageProxy
+  ) {
+    imageProxy.image?.let { image ->
+      val inputImage =
+        InputImage.fromMediaImage(
+          image,
+          imageProxy.imageInfo.rotationDegrees
+        )
 
-    fun setUpCamera(reactApplicationContext: ReactApplicationContext) {
-        if (allPermissionsGranted()) {
-            startCamera(reactApplicationContext)
-        }
+      barcodeScanner.process(inputImage)
+        .addOnSuccessListener { barcodeList ->
+          val barcode =
+            barcodeList.getOrNull(0) // `rawValue` is the decoded value of the barcode
 
-        cameraExecutor = Executors.newSingleThreadExecutor()
-
-        options = BarcodeScannerOptions.Builder()
-            .setBarcodeFormats(
-                Barcode.FORMAT_QR_CODE,
-                Barcode.FORMAT_AZTEC,
-                Barcode.FORMAT_CODE_128,
-                Barcode.FORMAT_CODE_39,
-                Barcode.FORMAT_CODE_93,
-                Barcode.FORMAT_CODABAR,
-                Barcode.FORMAT_DATA_MATRIX,
-                Barcode.FORMAT_EAN_13,
-                Barcode.FORMAT_EAN_8,
-                Barcode.FORMAT_ITF,
-                Barcode.FORMAT_PDF417,
-                Barcode.FORMAT_UPC_A,
-                Barcode.FORMAT_UPC_E
-            )
-            .build()
-        scanner = BarcodeScanning.getClient(options)
-
-        analysisUseCase.setAnalyzer(
-            // newSingleThreadExecutor() will let us perform analysis on a single worker thread
-            Executors.newSingleThreadExecutor()
-        ) { imageProxy ->
-            processImageProxy(scanner, imageProxy)
-        }
-    }
-
-    @SuppressLint("UnsafeOptInUsageError")
-    private fun processImageProxy(
-        barcodeScanner: BarcodeScanner,
-        imageProxy: ImageProxy
-    ) {
-        imageProxy.image?.let { image ->
-            val inputImage =
-                InputImage.fromMediaImage(
-                    image,
-                    imageProxy.imageInfo.rotationDegrees
+          // Log.e("adasdasd", barcodeList.getOrNull(0)?.cornerPoints.v);
+          barcode?.let { value ->
+            // mCameraProvider?.unbindAll() // this line will stop the camera from scanning after the first scan
+            val reactContext = context as ReactContext
+            val surfaceId = UIManagerHelper.getSurfaceId(reactContext)
+            val eventDispatcher: EventDispatcher? =
+              UIManagerHelper.getEventDispatcherForReactTag(
+                reactContext, id
+              )
+            eventDispatcher?.dispatchEvent(value.cornerPoints?.let {
+              value.boundingBox?.let { it1 ->
+                ReactNativeScannerViewEvent(surfaceId, id, it1,value.rawValue?:"",
+                  it
                 )
-
-            barcodeScanner.process(inputImage)
-                .addOnSuccessListener { barcodeList ->
-                    val barcode =
-                        barcodeList.getOrNull(0)        // `rawValue` is the decoded value of the barcode
-                    
-                    barcode?.rawValue?.let { value ->
-                        // mCameraProvider?.unbindAll() // this line will stop the camera from scanning after the first scan
-                        val reactContext = context as ReactContext
-                        val eventDispatcher: EventDispatcher? =
-                            UIManagerHelper.getEventDispatcherForReactTag(
-                                reactContext, id
-                            )
-                        eventDispatcher?.dispatchEvent(ReactNativeScannerViewEvent(id, value))
-                    }
-                }
-                .addOnFailureListener {
-                    // This failure will happen if the barcode scanning model
-                    // fails to download from Google Play Services
-                }.addOnCompleteListener {
-                    // When the image is from CameraX analysis use case, must
-                    // call image.close() on received images when finished
-                    // using them. Otherwise, new images may not be received
-                    // or the camera may stall.
-                    imageProxy.image?.close()
-                    imageProxy.close()
-                }
+              }
+            })
+          }
+        }
+        .addOnFailureListener {
+          // This failure will happen if the barcode scanning model
+          // fails to download from Google Play Services
+        }.addOnCompleteListener {
+          // When the image is from CameraX analysis use case, must
+          // call image.close() on received images when finished
+          // using them. Otherwise, new images may not be received
+          // or the camera may stall.
+          imageProxy.image?.close()
+          imageProxy.close()
         }
     }
+  }
 
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            context, it
-        ) == PackageManager.PERMISSION_GRANTED
-    }
+  private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+    ContextCompat.checkSelfPermission(
+      context, it
+    ) == PackageManager.PERMISSION_GRANTED
+  }
 
-    private fun startCamera(reactApplicationContext: ReactApplicationContext) {
+  private fun startCamera(reactApplicationContext: ReactApplicationContext) {
 
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+    val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
 
-        cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-            mCameraProvider = cameraProvider
-            // Preview
-            val surfacePreview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(preview.surfaceProvider)
-                }
+    cameraProviderFuture.addListener({
+      // Used to bind the lifecycle of cameras to the lifecycle owner
+      val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+      mCameraProvider = cameraProvider
+      // Preview
+      val surfacePreview = Preview.Builder()
+        .build()
+        .also {
+          it.setSurfaceProvider(preview.surfaceProvider)
+        }
 
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+      // Select back camera as a default
+      val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
+      try {
+        // Unbind use cases before rebinding
+        cameraProvider.unbindAll()
 
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    (reactApplicationContext.currentActivity as AppCompatActivity),
-                    cameraSelector,
-                    surfacePreview,
-                    analysisUseCase
-                )
+        // Bind use cases to camera
+        cameraProvider.bindToLifecycle(
+          (reactApplicationContext.currentActivity as AppCompatActivity),
+          cameraSelector,
+          surfacePreview,
+          analysisUseCase
+        )
 
-            } catch (exc: Exception) {
-                
-            }
+      } catch (exc: Exception) {
 
-        }, ContextCompat.getMainExecutor(context))
-    }
+      }
+
+    }, ContextCompat.getMainExecutor(context))
+  }
 }

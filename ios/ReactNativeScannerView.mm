dd613fe663c7fd8af321f170fba5509e268beb18
Ravindra Gupta <ravindra.gupta@capgemini.com>
2023-08-25 10:24:06 +0530

    Update Scanner Native Module to expose barcode bounds and corners

--- ios/ReactNativeScannerView.mm
+++ ios/ReactNativeScannerView.mm
@@ -30,68 +30,95 @@ using namespace facebook::react;
 
 - (instancetype)initWithFrame:(CGRect)frame
 {
-  if (self = [super initWithFrame:frame]) {
-    static const auto defaultProps = std::make_shared<const ReactNativeScannerViewProps>();
-    _props = defaultProps;
-
-    _view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 500, 500)];
-
-
-    _session = [[AVCaptureSession alloc] init];
-    _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
-    NSError *error = nil;
-
-    _input = [AVCaptureDeviceInput deviceInputWithDevice:_device error:&error];
-    if (_input) {
-        [_session addInput:_input];
-    } else {
-        NSLog(@"%@", [error localizedDescription]);
+    if (self = [super initWithFrame:frame]) {
+        static const auto defaultProps = std::make_shared<const ReactNativeScannerViewProps>();
+        _props = defaultProps;
+        
+        _view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 500, 500)];
+        
+        
+        _session = [[AVCaptureSession alloc] init];
+        _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        NSError *error = nil;
+        
+        _input = [AVCaptureDeviceInput deviceInputWithDevice:_device error:&error];
+        if (_input) {
+            [_session addInput:_input];
+        } else {
+            NSLog(@"%@", [error localizedDescription]);
+        }
+        
+        _output = [[AVCaptureMetadataOutput alloc] init];
+        [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+        [_session addOutput:_output];
+        
+        _output.metadataObjectTypes = [_output availableMetadataObjectTypes];
+        
+        _prevLayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
+        
+        _prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        [_view.layer addSublayer:_prevLayer];
+        [_session startRunning];
+        
+        self.contentView = _view;
     }
-
-    _output = [[AVCaptureMetadataOutput alloc] init];
-    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
-    [_session addOutput:_output];
-
-    _output.metadataObjectTypes = [_output availableMetadataObjectTypes];
-
-    _prevLayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
-
-    _prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
-       [_view.layer addSublayer:_prevLayer];
-       [_session startRunning];
-
-    self.contentView = _view;
-  }
-
-  return self;
+    
+    return self;
 }
 
 - (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
 {
-  CGRect highlightViewRect = CGRectZero;
-  AVMetadataMachineReadableCodeObject *barCodeObject;
-  NSString *detectionString = nil;
-  NSArray *barCodeTypes = @[AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
-                            AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
-                            AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode];
-
-  for (AVMetadataObject *metadata in metadataObjects) {
-    for (NSString *type in barCodeTypes) {
-      if ([metadata.type isEqualToString:type]) {
-        barCodeObject = (AVMetadataMachineReadableCodeObject *)[_prevLayer transformedMetadataObjectForMetadataObject:(AVMetadataMachineReadableCodeObject *)metadata];
-        highlightViewRect = barCodeObject.bounds;
-        detectionString = [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
-        break;
-      }
+    if (_eventEmitter != nullptr) {
+        return;
     }
-    if (detectionString != nil)  {
-      if (_eventEmitter != nullptr) {
-        std::dynamic_pointer_cast<const facebook::react::ReactNativeScannerViewEventEmitter>(_eventEmitter)->onQrScanned(facebook::react::ReactNativeScannerViewEventEmitter::OnQrScanned{
-        .value = std::string([detectionString UTF8String])
-        });
-      }
+    
+    NSArray *barCodeTypes = @[AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
+                              AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
+                              AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode];
+    
+    for (AVMetadataObject *metadata in metadataObjects) {
+        BOOL isValidCode = false;
+        for (NSString *type in barCodeTypes) {
+            if ([metadata.type isEqualToString:type]) {
+                isValidCode = true;
+                break;
+            }
+        }
+        
+        if (isValidCode == true)  {
+            AVMetadataMachineReadableCodeObject *barCodeObject = (AVMetadataMachineReadableCodeObject *)[_prevLayer transformedMetadataObjectForMetadataObject:(AVMetadataMachineReadableCodeObject *)metadata];
+            CGRect highlightViewRect = barCodeObject.bounds;
+            NSArray *corners = barCodeObject.corners;
+            NSString *codeString = [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
+            
+            CGPoint *topLeft = nullptr, *bottomLeft = nullptr, *bottomRight = nullptr, *topRight = nullptr;
+            
+            if(corners.count == 4) {
+                CGPointMakeWithDictionaryRepresentation((CFDictionaryRef)corners[0], topLeft);
+                CGPointMakeWithDictionaryRepresentation((CFDictionaryRef)corners[1], bottomLeft);
+                CGPointMakeWithDictionaryRepresentation((CFDictionaryRef)corners[2], bottomRight);
+                CGPointMakeWithDictionaryRepresentation((CFDictionaryRef)corners[3], topRight);
+            }
+            
+            facebook::react::ReactNativeScannerViewEventEmitter::OnQrScannedBounds bounds = {
+                .width = highlightViewRect.size.width,
+                .height = highlightViewRect.size.height,
+                .origin = {
+                    .topLeft = {.x = topLeft->x, .y = topLeft->y},
+                    .bottomLeft = {.x = bottomLeft->x, .y = bottomLeft->y},
+                    .bottomRight = {.x = bottomRight->x, .y = bottomRight->y},
+                    .topRight = {.x = topRight->x, .y = topRight->y}
+                }
+            };
+            
+            std::dynamic_pointer_cast<const facebook::react::ReactNativeScannerViewEventEmitter>(_eventEmitter)->onQrScanned(facebook::react::ReactNativeScannerViewEventEmitter::OnQrScanned{
+                .bounds = bounds,
+                .type = std::string([metadata.type UTF8String]),
+                .data = std::string([codeString UTF8String]),
+                .target = std::int32_t([codeString lengthOfBytesUsingEncoding:NSUTF8StringEncoding])
+            });
+        }
     }
-  }
 }
 
 - (void)updateProps:(Props::Shared const &)props oldProps:(Props::Shared const &)oldProps
@@ -100,8 +127,8 @@ using namespace facebook::react;
 }
 
 - (void)updateLayoutMetrics:(const facebook::react::LayoutMetrics &)layoutMetrics oldLayoutMetrics:(const facebook::react::LayoutMetrics &)oldLayoutMetrics{
-  [super updateLayoutMetrics:layoutMetrics oldLayoutMetrics:oldLayoutMetrics];
-  _prevLayer.frame = [_view.layer bounds];
+    [super updateLayoutMetrics:layoutMetrics oldLayoutMetrics:oldLayoutMetrics];
+    _prevLayer.frame = [_view.layer bounds];
 }
 
 @end
@@ -110,3 +137,4 @@ Class<RCTComponentViewProtocol> ReactNativeScannerViewCls(void)
 {
     return ReactNativeScannerView.class;
 }
+
--- src/ReactNativeScannerViewNativeComponent.ts
+++ src/ReactNativeScannerViewNativeComponent.ts
@@ -1,9 +1,21 @@
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 import type { ViewProps } from 'react-native';
-import type { DirectEventHandler } from 'react-native/Libraries/Types/CodegenTypes';
+import type { DirectEventHandler, Int32, Double } from 'react-native/Libraries/Types/CodegenTypes';
 
 type Event = Readonly<{
-  value: string;
+  bounds: Readonly<{
+    width: Double,
+    height: Double,
+    origin: Readonly<{
+      topLeft: Readonly<{ x: Double, y: Double }>;
+      bottomLeft: Readonly<{ x: Double, y: Double }>;
+      bottomRight: Readonly<{ x: Double, y: Double }>;
+      topRight: Readonly<{ x: Double, y: Double }>;
+    }>
+  }>;
+  type: string;
+  data: string;
+  target: Int32;
 }>;
 
 interface NativeProps extends ViewProps {
